c3fbd7da6dfc99e0d6f36401b007ee5818e874ca
teste <sitarzdominik@gmail.com>
2025-09-04 14:19:24 -0300

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://api.leprive.fun";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://leprive.com.pl";
 
 export async function fetchFromStrapi(endpoint: string) {
   try {
